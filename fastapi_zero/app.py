360dfe37721341e7bc7a826110ec92ef4336e266
Lucas Vieira <lucas.vieira@ECO.LAN>
2025-08-14 18:14:08 -0300

    mais schemas...

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,14 +1,20 @@
-from fastapi import FastAPI
 from http import HTTPStatus
 
-from fastapi_zero.schemas import Message
+from fastapi import FastAPI
+
+from fastapi_zero.schemas import Message, UserPublic, UserSchema
 
 app = FastAPI(title='Minha Api bala')
 
+database = []
 
-@app.get(
-        '/',
-          status_code=HTTPStatus.OK,
-          response_model=Message)
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    # breakpoint() #para debug
+    return user
