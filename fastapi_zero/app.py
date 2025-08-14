e0b144829793171b8d15adaba96be9bab5ec2d88
Lucas Vieira <lucas.vieira@ECO.LAN>
2025-08-14 17:07:07 -0300

    Trabalhando com schemas

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from http import HTTPStatus
 
-app = FastAPI()
+from fastapi_zero.schemas import Message
+
+app = FastAPI(title='Minha Api bala')
 
 
-@app.get('/')
+@app.get(
+        '/',
+          status_code=HTTPStatus.OK,
+          response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
